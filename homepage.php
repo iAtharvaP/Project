3b6fc76b8d661ccf92daac575617d7ae4dba5f72
iAtharvaP <62158847+iAtharvaP@users.noreply.github.com>
2020-03-14 03:00:16 +0530

    initial commit

---
+++ homepage.php
@@ -0,0 +1,24 @@
+<?php
+	session_start();
+	require_once('dbconfig/config.php');
+?>
+
+<!DOCTYPE html>
+<head>
+<title>Home Page</title>
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+	<div id="main-wrapper">
+	
+	<div class="imgcontainer">
+			<img src="imgs/bg2.jpg" alt="Avatar" class="center">
+			</div>
+
+			
+		
+		
+	</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,87 @@
+<?php 
+  session_start();
+  require_once('dbconfig/config.php');
+ ?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Input Form UI Design</title>
+	<link rel="stylesheet"  href="css/style.css">
+</head>
+
+<body>
+	<div class="imgcontainer">
+				<img src="imgs/bg.jpg" alt="Avatar" class="center"  width="500">
+			</div>
+<div class="box">
+	<h2>LOGIN</h2>
+	
+	<form class="myform" action="Index.php" method="post">
+		<div class="inputBox">
+	          <input type="text" name="username" required="">
+	          <label>User ID</label>
+	      </div>
+
+<div class="inputBox">
+	          <input type="password" name="password" required="">
+	          <label>Password</label>
+	      </div>
+
+ <input type="submit" name="login" required="">	  
+
+
+	 </form>
+
+	<?php
+			if(isset($_POST['login']))
+			{
+
+				$flag=0;
+				@$username=$_POST['username'];
+				@$password=$_POST['password'];
+				$query ="select * from user where username='$username' and password='$password' ";
+				
+				$query_run = mysqli_query($con,$query);
+				
+
+                $flag=$flag+1;
+                $query1 ="insert into user2 (username1,flag) values($username,0)";
+                $query1_run = mysqli_query($con,$query1);
+
+
+
+
+
+				if($query_run)
+				{
+					if(mysqli_num_rows($query_run)>0)
+					{
+						
+					$row = mysqli_fetch_array($query_run,MYSQLI_ASSOC);
+					
+					$_SESSION['username'] = $username;
+					$_SESSION['password'] = $password;
+					
+					header( "Location: homepage.php");
+					}
+					else
+					{
+						echo '<script type="text/javascript">alert("No such User exists. Invalid Credentials")</script>';
+					}
+				}
+				else
+				{
+					echo '<script type="text/javascript">alert("Database Error")</script>';
+				}
+			}
+			else
+			{
+			}
+		?>
+
+   </div>
+ </body>
+</html>
